85dad88c547b877dc4691036e44e1d9767033ae4
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 20:23:47 -0300

    estrutura inicial do projeto

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package br.com.fiapride.model;
+
+        public class veiculos {
+            public String individuo;
+            public String pl;
+            public int gas;
+            public void adicionar(int v) {
+                = gas + v;
+
+                public void gasta(double v) {
+        gas = gas - v;
+        }
+}
---
+++ src/br/com/fiapride/model/Veiculo.java
@@ -0,0 +1,13 @@
+package br.com.fiapride.model;
+
+public class Veiculo {
+    private String proprietario;
+    private String placa;
+    private double combustivel;
+    private void adicionar(int v) {
+         combustivel = combustivel + v;
+         }
+         public void gasta(double v) {
+
+         }
+ }
