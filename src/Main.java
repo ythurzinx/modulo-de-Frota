81b71325a6968f3da7e85bc14b72fd004c428f70
Arthur Alencar <alencararthur47@gmail.com>
2026-03-17 20:35:35 -0300

    cria classe Veiculo com atributos e metodos encapsulamento conforme nossa aula 3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,1 @@
-package br.com.fiapride.model;
-
-        public class veiculos {
-            public String individuo;
-            public String pl;
-            public int gas;
-            public void adicionar(int v) {
-                = gas + v;
-
-                public void gasta(double v) {
-        gas = gas - v;
-        }
-}
+package br.com.fiapride.model.
--- src/br/com/fiapride/model/Veiculo.java
+++ src/br/com/fiapride/model/Veiculo.java
@@ -1,13 +1,59 @@
 package br.com.fiapride.model;
 
 public class Veiculo {
+
     private String proprietario;
     private String placa;
     private double combustivel;
-    private void adicionar(int v) {
-         combustivel = combustivel + v;
-         }
-         public void gasta(double v) {
 
-         }
- }
+    public Veiculo(String proprietario, String placa) {
+        this.setProprietario(proprietario);
+        this.setPlaca(placa);
+        this.setCombustivel(0);
+    }
+    public String getProprietario() {
+        return this.proprietario;
+    }
+    private void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+    public String getPlaca() {
+        return this.placa;
+    }
+
+    private void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public double getCombustivel() {
+        return this.combustivel;
+    }
+
+    private void setCombustivel(double combustivel) {
+        if (combustivel >= 0) {
+            this.combustivel = combustivel;
+        } else {
+            System.out.println("Valor inválido");
+        }
+    }
+
+    public void adicionarCombustivel(double quantidade) {
+        if (quantidade > 0) {
+            this.setCombustivel(this.getCombustivel() + quantidade);
+        } else {
+            System.out.println("Quantidade inválida");
+        }
+    }
+
+    public void consumirCombustivel(double quantidade) {
+        if (quantidade > 0) {
+            if (this.getCombustivel() >= quantidade) {
+                this.setCombustivel(this.getCombustivel() - quantidade);
+            } else {
+                System.out.println("Combustível insuficiente");
+            }
+        } else {
+            System.out.println("Quantidade inválida");
+        }
+    }
+}
